971a2c40f7383307ddcd94b2e19c0a41f95db20d
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-24 19:40:10 +0530

    Committing files

---
+++ CodeChef/C++14/POLYREL/37093874.cpp
@@ -0,0 +1,88 @@
+
+ #include<bits/stdc++.h>
+#include<map>
+
+using namespace std;
+ 
+#define int long long
+ 
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("inpu1t.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+
+ 
+ 
+int32_t main() 
+{ 
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+ //anmol();
+ int t;
+ cin>>t;
+ //int j =1;
+ while(t--){
+
+int n;
+cin>>n;
+
+
+for(int i =0; i<n; i++){
+
+ int x,y;
+ cin>>x>>y;
+
+
+
+
+}
+
+int count =0;
+//int sum =n;
+if(n<=4){
+
+  cout<<n<<endl;
+  continue;
+}
+
+int sum =0;
+
+while(n>4){
+
+sum +=n;
+n = n/2;
+
+
+
+
+}
+if(n==3 || n==4){
+
+  sum+=n;
+}
+
+
+cout<<sum<<endl;
+//int a[n];
+
+}
+}
+
+ 
+
+
+
+ 
+
+
+ 
+    
+    
